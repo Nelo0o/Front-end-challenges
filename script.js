506366da557342b1c6d44583241c7d7c0be46d9e
Nelo07o <90777157+Nelo07o@users.noreply.github.com>
2023-05-04 18:15:18 +0200

    Update landing page

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 const folders = [
-  "Interactive-rating-component",
-  "NFT-preview-card-component",
-  "Order-summary-component",
-  "Profile-card-component",
-  "Product-preview-card-component",
-  "QR-code-card",
-  "Results-summary-component",
-  "Stats-preview-card-component",
-  "3-column-preview-card-component",
-  "Social-proof-section",
+    "Interactive-rating-component",
+    "NFT-preview-card-component",
+    "Order-summary-component",
+    "Profile-card-component",
+    "Product-preview-card-component",
+    "QR-code-card",
+    "Results-summary-component",
+    "Stats-preview-card-component",
+    "Social-proof-section",
+    "3-column-preview-card-component",
 ];
-const list = document.querySelector("ul");
 
 folders.forEach((element) => {
-  const newLi = document.createElement("li");
-  const newLink = document.createElement("a");
-  newLink.text = element;
-  newLink.href = `./${element}/index.html`;
-  newLi.appendChild(newLink);
-  list.appendChild(newLi);
-});
+    const section = document.querySelector(".projects");
+
+    const newCard = document.createElement("article");
+    const newContainerImg = document.createElement("div");
+    const newImg = document.createElement("img");
+    const newContainerInfos = document.createElement("div");
+    const newTitle = document.createElement("h2");
+    const newButton = document.createElement("button");
+    const newLink = document.createElement("a");
+
+    newCard.className = "card";
+
+    newContainerImg.className = "container-screen";
+    newImg.src = `./${element}/images/screenshot.png`;
+    newImg.alt = `${element} screenshot`;
+
+    newContainerInfos.className = "container-infos";
+    newTitle.innerText = element.replace(/-/g, " ");
+    newLink.innerText = "Preview";
+    newLink.href = `./${element}/index.html`;
+    newLink.target = "_blank";
+    newButton.appendChild(newLink);
+
+    newContainerImg.appendChild(newImg);
+    newContainerInfos.appendChild(newTitle);
+    newContainerInfos.appendChild(newButton);
+
+    newCard.appendChild(newContainerImg);
+    newCard.appendChild(newContainerInfos);
+
+    section.appendChild(newCard);
+})
+
+
